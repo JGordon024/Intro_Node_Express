0d0ded087da957cdb98b9c2d160aaa48e23ba358
Jacob Gordon <jgordon024@west-mec.org>
2019-10-09 18:22:38 -0700

    Current change
    Fixed current change

--- Projects/server.js
+++ Projects/server.js
@@ -6,18 +6,8 @@ const port = 3000;
 const path = require('path');
 const router = express.Router;
 
-<<<<<<< HEAD
 app.set('views', path.join(__dirname,'views'))
 app.set('view engine', 'pug')
-=======
-app.use(function(err, req, res, next) {
-  if(err) {
-    res.status(err.status || 500)
-      .type('txt')
-      .send(err.message || 'SERVER ERROR');
-  }  
-})
->>>>>>> master
 
 app.use(express.static("public"))
 app.get("/", (req, res) => res.render('index'))
