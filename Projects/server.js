c749e8c8b6faf226c5f3363e3ff4f7bbd7e1f817
Jacob Gordon <jgordon024@west-mec.org>
2019-10-10 18:21:01 -0700

    Created public file, made routes
    I created a public file and moved files there to be used through static in express and correctly called. I also fixed the pug files so that the routes are linked correctly.

--- Projects/server.js
+++ Projects/server.js
@@ -9,7 +9,23 @@ const router = express.Router;
 app.set('views', path.join(__dirname,'views'))
 app.set('view engine', 'pug')
 
-app.use(express.static("public"))
-app.get("/", (req, res) => res.render('index'))
+app.use(express.static("public"));
+
+app.get("/fire", (req, res) => {
+    
+    fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+    .then(res => res.json)
+    .then(
+        data => {
+            console.log(data.name)
+            res.render('fire',{pokemon: data.name})
+        }
+    )
+})
+app.get("/water", (req, res) => res.render('water'))
+app.get("/electric", (req,res) => res.render('electric'))
+app.get("/dragon", (req, res) => res.render("dragon"))
+app.get("/earth", (req, res)=>res.render("earth"))
+
 
 app.listen(port, ()=>console.log(`The server is ${port}`));
