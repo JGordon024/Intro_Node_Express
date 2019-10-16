c6620e6a395d8995340008627fe3fc74844aaf99
Jacob Gordon <jgordon024@west-mec.org>
2019-10-15 18:27:55 -0700

    Fixed all variables
    Made all appropiate variables, trying to fix the issues with using the render for all 3 APIs

--- Projects/server.js
+++ Projects/server.js
@@ -40,32 +40,31 @@ app.get("/fire", (req, res) => {
         } 
     )
 
-    // fetch('https://pokeapi.co/api/v2/pokemon/growlithe')
-    // .then(res => res.json())
-    // .then(
-    //     data => {
-    //         const {...img} = data.sprites
-    //         res.render('fire',{
-    //             growName: data.name,
-    //             growWeight: data.weight,
-    //             growHeight: data.height,
-    //             growImg: img.front_default
-    //         })
-    //     } 
-    // )
-})
-app.get("/water", (req, res) => {
-    res.render('water')
-    fetch('https://pokeapi.co/api/v2/pokemon/squirtle')
+    fetch('https://pokeapi.co/api/v2/pokemon/growlithe')
     .then(res => res.json())
     .then(
         data => {
             const {...img} = data.sprites
             res.render('fire',{
-                charName: data.name,
-                charWeight: data.weight,
-                charHeight: data.height,
-                charImg: img.front_default
+                growName: data.name,
+                growWeight: data.weight,
+                growHeight: data.height,
+                growImg: img.front_default
+            })
+        } 
+    )
+})
+app.get("/water", (req, res) => {
+    fetch('https://pokeapi.co/api/v2/pokemon/squirtle')
+    .then(res => res.json())
+    .then(
+        data => {
+            const {...img} = data.sprites
+            res.render('water',{
+                squirtName: data.name,
+                squirtWeight: data.weight,
+                squirtHeight: data.height,
+                squirtImg: img.front_default
             })
         } 
     )
@@ -75,11 +74,11 @@ app.get("/water", (req, res) => {
     .then(
         data => {
             const {...img} = data.sprites
-            res.render('fire',{
-                charName: data.name,
-                charWeight: data.weight,
-                charHeight: data.height,
-                charImg: img.front_default
+            res.render('water',{
+                buizName: data.name,
+                buizWeight: data.weight,
+                buizHeight: data.height,
+                buizImg: img.front_default
             })
         } 
     )
@@ -89,42 +88,84 @@ app.get("/water", (req, res) => {
     .then(
         data => {
             const {...img} = data.sprites
-            res.render('fire',{
-                charName: data.name,
-                charWeight: data.weight,
-                charHeight: data.height,
-                charImg: img.front_default
+            res.render('water',{
+                gyarName: data.name,
+                gyarWeight: data.weight,
+                gyarHeight: data.height,
+                gyarImg: img.front_default
             })
         } 
     )
 
 })
 app.get("/electric", (req,res) => {
-    res.render('electric')
     fetch('https://pokeapi.co/api/v2/pokemon/pikachu')
     .then(res => res.json())
     .then(
         data => {
             const {...img} = data.sprites
-            res.render('fire',{
-                charName: data.name,
-                charWeight: data.weight,
-                charHeight: data.height,
-                charImg: img.front_default
+            res.render('electric',{
+                pikName: data.name,
+                pikWeight: data.weight,
+                pikHeight: data.height,
+                pikImg: img.front_default
             })
         } 
     )
 
-    fetch('https://pokeapi.co/api/v2/pokemon/garchomp')
+    fetch('https://pokeapi.co/api/v2/pokemon/zapdos')
     .then(res => res.json())
     .then(
         data => {
             const {...img} = data.sprites
-            res.render('fire',{
-                charName: data.name,
-                charWeight: data.weight,
-                charHeight: data.height,
-                charImg: img.front_default
+            res.render('electric',{
+                zapName: data.name,
+                zapWeight: data.weight,
+                zapHeight: data.height,
+                zapImg: img.front_default
+            })
+        } 
+    )
+    
+    fetch('https://pokeapi.co/api/v2/pokemon/magnezone')
+    .then(res => res.json())
+    .then(
+        data => {
+            const {...img} = data.sprites
+            res.render('electric',{
+                magName: data.name,
+                magWeight: data.weight,
+                magHeight: data.height,
+                magImg: img.front_default
+            })
+        } 
+    )
+    
+})
+app.get("/dragon", (req, res) => {
+    fetch('https://pokeapi.co/api/v2/pokemon/dragonite')
+    .then(res => res.json())
+    .then(
+        data => {
+            const {...img} = data.sprites
+            res.render('dragon',{
+                dragName: data.name,
+                dragWeight: data.weight,
+                dragHeight: data.height,
+                dragImg: img.front_default
+            })
+        } 
+    )
+    fetch('https://pokeapi.co/api/v2/pokemon/salamence')
+    .then(res => res.json())
+    .then(
+        data => {
+            const {...img} = data.sprites
+            res.render('dragon',{
+                salaName: data.name,
+                salaWeight: data.weight,
+                salaHeight: data.height,
+                salaImg: img.front_default
             })
         } 
     )
@@ -134,72 +175,56 @@ app.get("/electric", (req,res) => {
     .then(
         data => {
             const {...img} = data.sprites
-            res.render('fire',{
-                charName: data.name,
-                charWeight: data.weight,
-                charHeight: data.height,
-                charImg: img.front_default
-            })
-        } 
-    )
-})
-app.get("/dragon", (req, res) => {
-    res.render("dragon")
-    fetch('https://pokeapi.co/api/v2/pokemon/dragonite')
-    .then(res => res.json())
-    .then(
-        data => {
-            const {...img} = data.sprites
-            res.render('fire',{
-                charName: data.name,
-                charWeight: data.weight,
-                charHeight: data.height,
-                charImg: img.front_default
+            res.render('dragon',{
+                rayName: data.name,
+                rayWeight: data.weight,
+                rayHeight: data.height,
+                rayImg: img.front_default
             })
         } 
     )
 })
 app.get("/earth", (req, res)=>{
-    res.render("earth")
 
-    fetch('https://pokeapi.co/api/v2/pokemon/electabuzz')
+    fetch('https://pokeapi.co/api/v2/pokemon/rhyhorn')
     .then(res => res.json())
     .then(
         data => {
             const {...img} = data.sprites
-            res.render('fire',{
-                charName: data.name,
-                charWeight: data.weight,
-                charHeight: data.height,
-                charImg: img.front_default
+            res.render('earth',{
+                
+                rhyName: data.name,
+                rhyWeight: data.weight,
+                rhyHeight: data.height,
+                rhyImg: img.front_default
             })
         } 
     )
 
-    fetch('https://pokeapi.co/api/v2/pokemon/electabuzz')
+    fetch('https://pokeapi.co/api/v2/pokemon/geodude')
     .then(res => res.json())
     .then(
         data => {
             const {...img} = data.sprites
-            res.render('fire',{
-                charName: data.name,
-                charWeight: data.weight,
-                charHeight: data.height,
-                charImg: img.front_default
+            res.render('earth',{
+                geoName: data.name,
+                geoWeight: data.weight,
+                geoHeight: data.height,
+                geoImg: img.front_default
             })
         } 
     )
 
-    fetch('https://pokeapi.co/api/v2/pokemon/electabuzz')
+    fetch('https://pokeapi.co/api/v2/pokemon/aerodactyl')
     .then(res => res.json())
     .then(
         data => {
             const {...img} = data.sprites
-            res.render('fire',{
-                charName: data.name,
-                charWeight: data.weight,
-                charHeight: data.height,
-                charImg: img.front_default
+            res.render('earth',{
+                aeroName: data.name,
+                aeroWeight: data.weight,
+                aeroHeight: data.height,
+                aeroImg: img.front_default
             })
         } 
     )
